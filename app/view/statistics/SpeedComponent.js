d4567599de6351d26c83a9e7ca087f9dc41b1799
Craig Whitcombe <craig.whitcombe@gmail.com>
2013-04-10 19:58:51 +0200

    using absolute layout for speedcomponent to stop resizing when text strings are large

--- app/view/statistics/SpeedComponent.js
+++ app/view/statistics/SpeedComponent.js
@@ -3,28 +3,33 @@ Ext.define('Clutch.view.statistics.SpeedComponent', {
     extend : 'Ext.container.Container',
 
     alias : 'widget.speedcomponent',
-    
-     
+
     config : {
         speedUp : 0,
         speedDown : 0
     },
-    layout : 'hbox',
+    layout : 'absolute',
 
     items : [{
         xtype : 'menuitem',
-        text : 'Down: '
+        text : 'Down: ',
+        x : 0
     }, {
         xtype : 'menuitem',
         itemId : 'speeddown',
-        //width : 80
+        x : 50
     }, {
         xtype : 'menuitem',
-        text : 'Up: '
-    },{
+        text : 'Up: ',
+        x : 150
+    }, {
         xtype : 'menuitem',
         itemId : 'speedup',
-        // width : 80
+        x : 200
+    }, {
+        xtype : 'tbspacer',
+        x : 300,
+        width : 50
     }],
 
     constructor : function(cfg) {
@@ -34,14 +39,16 @@ Ext.define('Clutch.view.statistics.SpeedComponent', {
 
     applySpeedUp : function(v, oldValue) {
         var corrected = Ext.util.Format.fileSize(v);
-        if (corrected !== '-') corrected += '/s';
+        if (corrected !== '-')
+            corrected += '/s';
         this.down('#speedup').setText(corrected);
         return v;
     },
 
     applySpeedDown : function(v, oldValue) {
         var corrected = Ext.util.Format.fileSize(v);
-        if (corrected !== '-') corrected += '/s';
+        if (corrected !== '-')
+            corrected += '/s';
         this.down('#speeddown').setText(corrected);
         return v;
     }

3d9c8b4bd4778fab2100c39dad8fb6e955ec473d
Craig Whitcombe <craig.whitcombe@gmail.com>
2013-04-12 14:21:28 +0200

    fixed cosmetic issue with the main toolbar (buttongroups had a border i couldnt get rid of, just decided not to use buttongroups at all

--- app/view/MainToolbar.js
+++ app/view/MainToolbar.js
@@ -8,122 +8,100 @@ Ext.define("Clutch.view.MainToolbar", {
     frame : false,
 
     items : [{
-        xtype : 'buttongroup',
-        columns : 5,
-        frame : false,
-        defaults : {
-            border : false
 
-        },
-
-        items : [{
-
-            xtype : 'splitbutton',
-            text : 'Add torrent',
-            scale : 'large',
-            rowspan : 3,
-            iconCls : 'add',
-            iconAlign : 'top',
-            arrowAlign : 'right',
-            action : 'add-url',
-            menu : [{
-                text : 'Add via URL',
-                itemId : 'btnAddViaUrl',
-                iconCls : 'add'
-            }, {
-                text : 'Upload .torrent file',
-                itemId : 'btnUploadTorrent',
-                iconCls : 'add'
-            }]
+        xtype : 'splitbutton',
+        text : 'Add torrent',
+        scale : 'large',
+        rowspan : 3,
+        iconCls : 'add',
+        iconAlign : 'top',
+        arrowAlign : 'right',
+        action : 'add-url',
+        menu : [{
+            text : 'Add via URL',
+            itemId : 'btnAddViaUrl',
+            iconCls : 'add'
         }, {
-            xtype : 'splitbutton',
-            text : 'Remove',
-            scale : 'large',
-            rowspan : 3,
-            iconCls : 'remove',
-            iconAlign : 'top',
-            arrowAlign : 'right',
-            action : 'remove-selected',
-            menu : [{
-                text : 'Remove selected',
-                itemId : 'btnRemoveSelected',
-                iconCls : 'remove-small',
-                action : 'remove-selected'
-            }, {
-                text : 'Remove all',
-                itemId : 'btnRemoveAll',
-                iconCls : 'remove-small',
-                action : 'remove-all'
-            }, {
-                text : 'Remove finished',
-                itemId : 'btnRemoveFinished',
-                iconCls : 'remove-small',
-                action : 'remove-finished'
-            }]
+            text : 'Upload .torrent file',
+            itemId : 'btnUploadTorrent',
+            iconCls : 'add'
+        }]
+    }, {
+        xtype : 'splitbutton',
+        text : 'Remove',
+        scale : 'large',
+        rowspan : 3,
+        iconCls : 'remove',
+        iconAlign : 'top',
+        arrowAlign : 'right',
+        action : 'remove-selected',
+        menu : [{
+            text : 'Remove selected',
+            itemId : 'btnRemoveSelected',
+            iconCls : 'remove-small',
+            action : 'remove-selected'
         }, {
-            xtype : 'splitbutton',
-            text : 'Pause',
-            scale : 'large',
-            rowspan : 3,
-            itemId : 'btnPause',
-            iconAlign : 'top',
-            arrowAlign : 'right',
+            text : 'Remove all',
+            itemId : 'btnRemoveAll',
+            iconCls : 'remove-small',
+            action : 'remove-all'
+        }, {
+            text : 'Remove finished',
+            itemId : 'btnRemoveFinished',
+            iconCls : 'remove-small',
+            action : 'remove-finished'
+        }]
+    }, {
+        xtype : 'splitbutton',
+        text : 'Pause',
+        scale : 'large',
+        rowspan : 3,
+        itemId : 'btnPause',
+        iconAlign : 'top',
+        arrowAlign : 'right',
+        iconCls : 'pause',
+        action : 'pause-selected',
+        menu : [{
+            text : 'Pause All',
+            itemId : 'btnPauseAll',
             iconCls : 'pause',
-            action : 'pause-selected',
-            menu : [{
-                text : 'Pause All',
-                itemId : 'btnPauseAll',
-                iconCls : 'pause',
-                action : 'pause-all'
-            }]
-        }, {
-            xtype : 'splitbutton',
-            text : 'Resume',
-            itemId : 'btnResume',
-            scale : 'large',
-            rowspan : 3,
-            iconAlign : 'top',
-            arrowAlign : 'right',
-            iconCls : 'resume',
-            action : 'resume-selected',
-            menu : [{
-                text : 'Resume All',
-                itemId : 'btnResumeAll',
-                iconCls : 'resume-all',
-                action : 'resume-all'
-            }]
+            action : 'pause-all'
         }]
-
+    }, {
+        xtype : 'splitbutton',
+        text : 'Resume',
+        itemId : 'btnResume',
+        scale : 'large',
+        rowspan : 3,
+        iconAlign : 'top',
+        arrowAlign : 'right',
+        iconCls : 'resume',
+        action : 'resume-selected',
+        menu : [{
+            text : 'Resume All',
+            itemId : 'btnResumeAll',
+            iconCls : 'resume-all',
+            action : 'resume-all'
+        }]
     }, '->', {
-        xtype : 'buttongroup',
-        frame : false,
-        columns : 3,
-        defaults : {
-            border : false
+        text : 'Statistics',
+        itemId : 'btnStatistics',
+        iconCls : 'info',
+        scale : 'large',
+        rowspan : 3,
+        iconAlign : 'top',
+        arrowAlign : 'right',
+        action : 'show-statistics'
 
-        },
-        items : [{
-            text : 'Statistics',
-            itemId : 'btnStatistics',
-            iconCls : 'info',
-            scale : 'large',
-            rowspan : 3,
-            iconAlign : 'top',
-            arrowAlign : 'right',
-            action : 'show-statistics'
-
-        }, {
-            text : 'Settings',
-            itemId : 'btnSettings',
-            iconCls : 'settings',
-            scale : 'large',
-            rowspan : 3,
-            iconAlign : 'top',
-            arrowAlign : 'right',
-            action : 'show-settings'
-
-        }]
+    }, {
+        text : 'Settings',
+        itemId : 'btnSettings',
+        iconCls : 'settings',
+        scale : 'large',
+        rowspan : 3,
+        iconAlign : 'top',
+        arrowAlign : 'right',
+        action : 'show-settings'
 
     }]
-
 });
